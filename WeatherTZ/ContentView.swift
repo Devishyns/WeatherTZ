06e895886962f0ef23f082cbcdf09c92738f4028
Yunus <devishyns@gmail.com>
2026-02-12 12:47:59 +0300

    Initial Commit

---
+++ WeatherTZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherTZ
+//
+//  Created by yunus on 12.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherTZ/WeatherTZApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTZApp.swift
+//  WeatherTZ
+//
+//  Created by yunus on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
